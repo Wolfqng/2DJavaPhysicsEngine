48f6c9ccb23a226f3710b46e6881616080fc8452
Wolfqng <tkelly6397@gmail.com>
2021-01-11 17:15:39 -0500

    Added outer collision box detection

--- physicsEngine/src/physicsEngine/Enviroment.java
+++ physicsEngine/src/physicsEngine/Enviroment.java
@@ -9,15 +9,21 @@ import java.awt.TexturePaint;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import physicsEngine.events.CollisionEvent;
+import physicsEngine.events.CollisionPointEvent;
+
 public class Enviroment extends JPanel {
 	private static final long serialVersionUID = 1L;
 	public static ArrayList<Polygon> objects = new ArrayList<>();
 	BufferedImage s;
+	public Queue<double[]> tp = new LinkedList<double[]>();
 	
 	public Enviroment() {
 		try {
@@ -38,12 +44,17 @@ public class Enviroment extends JPanel {
 	    }
 	    
 	    g2d.setColor(Color.BLACK);
-	    g2d.setStroke(new BasicStroke(5));
-	    g2d.drawRect(20, 20, 80, 80);
+	    //Width 5, JOIN_BEVEL
+	    BasicStroke stroke = new BasicStroke(5, 2, 2);
+	    g2d.setStroke(stroke);
+	    //g2d.drawRect(20, 20, 80, 80);
 	    for(Polygon obj : objects) {
 	    	g2d.setColor(obj.getC());
-	    	obj.rotate(5);
-	    	g2d.drawPolygon(Polygon.copyFromDoubleArray(obj.getxPoints()), Polygon.copyFromDoubleArray(obj.getyPoints()), obj.getxPoints().length);
+	    	//obj.rotate(5);
+	    	g2d.drawPolygon(Polygon.doubleToIntArray(obj.getxPoints()), Polygon.doubleToIntArray(obj.getyPoints()), obj.getxPoints().length);
+	    	
+	    	generateOuterCollider(g2d, obj);
+	    	//generateTrails(g2d, obj);
 	    }
 	    
 	  }
@@ -52,11 +63,56 @@ public class Enviroment extends JPanel {
 		JFrame frame = new JFrame("Textures");
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    frame.add(new Enviroment());
-	    frame.setSize(1296, 935);  //Should be 1280, 896 although
+	    frame.setSize(1280 + 16, 896 + 39);  //Should be 1280, 896 although
 	    frame.setVisible(true);
 	    
-	    double[] xp = new double[]{50, 90, 90, 50};
-	    double[] yp = new double[]{50, 50, 90, 90};
-	    objects.add(new Polygon(xp, yp, 5, 5, Color.black));
+	    double[] xp = new double[]{90, 90, 200, 200};
+	    double[] yp = new double[]{90, 200, 300, 90};
+	    objects.add(new Polygon(xp, yp, 10, 5, Color.black));
+	    int xoff = 200;
+	    double[] xp1 = new double[]{100 + xoff, 140 + xoff, 210 + xoff, 210 + xoff, 300 + xoff};
+	    double[] yp2 = new double[]{50, 200, 360, 90, 50};
+	    objects.add(new Polygon(xp1, yp2, 10, 5, Color.green));
+	    
+	    Object monitor = new Object();
+        synchronized(monitor) {
+            while(true) {
+                frame.repaint();
+                for(Polygon p : objects) {
+                	p.update(); //Check for interactions
+                	p.rotate(.1);
+                }
+                try{Thread.sleep(1);}catch(InterruptedException ex){Thread.currentThread().interrupt();}
+            }
+        }
 	}
+	
+	//generate trail points
+    public void generateTrails(Graphics2D g2d, Polygon obj) {
+    	g2d.setPaint(new Color(255, 0, 0, 100));
+		for(int i = 0; i < obj.getxPoints().length; i++) {
+			double[] coord = new double[2];
+			coord[0] = obj.getxPoints()[i];
+			coord[1] = obj.getyPoints()[i];
+			tp.add(coord);
+		}
+    	
+    	
+    	if(tp.size() > 10000) tp.remove();
+    	ArrayList<double[]> coords = new ArrayList<double[]>(tp);
+    	for(double[] coord : coords) 
+    		g2d.fillRect((int)coord[0], (int)coord[1], 1, 1);
+    }
+    
+    public void generateOuterCollider(Graphics2D g2d, Polygon obj) {
+    	g2d.setPaint(new Color(255, 100, 255, 100));
+    	double[][] coords = obj.outerColliderPoints();
+    	g2d.drawPolygon(Polygon.doubleToIntArray(coords[0]), Polygon.doubleToIntArray(coords[1]), coords[0].length);
+    	
+    	ArrayList<CollisionEvent> collisions = obj.getOuterCollisions();
+    	if(!collisions.isEmpty()) 
+        	g2d.fillPolygon(Polygon.doubleToIntArray(coords[0]), Polygon.doubleToIntArray(coords[1]), coords[0].length);
+    	
+    }
+    
 }
--- physicsEngine/src/physicsEngine/Polygon.java
+++ physicsEngine/src/physicsEngine/Polygon.java
@@ -1,6 +1,9 @@
 package physicsEngine;
 
 import java.awt.Color;
+import java.util.ArrayList;
+
+import physicsEngine.events.CollisionEvent;
 
 public class Polygon {
 	private double[] xPoints;
@@ -15,6 +18,7 @@ public class Polygon {
 		this.rotation = rotation;
 		this.mass = mass;
 		this.c = c;
+		rotate(rotation);
 	}
 	
 	public void update() {
@@ -23,17 +27,24 @@ public class Polygon {
 		//Update position 
 	}
 	
-	public void rotate(int deg) {
+	public void rotate(double deg) {
 		double rad = Math.toRadians(deg);
+		//System.out.println(deg + " === " + rad);
+		double[] cenPts = findCenter(this.xPoints, this.yPoints);
 		for(int i = 0; i < xPoints.length; i++) {
 			double x = this.xPoints[i];
 			double y = this.yPoints[i];
-			this.xPoints[i] = (x * Math.cos(rad)) - (y * Math.sin(rad));
-			this.yPoints[i] = (y * Math.cos(rad)) + (x * Math.sin(rad));
+			
+			x -= cenPts[0];
+			y -= cenPts[1];
+			
+			this.xPoints[i] = (x * Math.cos(rad)) - (y * Math.sin(rad)) + cenPts[0];
+			this.yPoints[i] = (y * Math.cos(rad)) + (x * Math.sin(rad)) + cenPts[1];
 		}
 	}
 	
-	public static int[] copyFromDoubleArray(double[] source) {
+	//converts double array to an integer array
+	public static int[] doubleToIntArray(double[] source) {
 	    int[] dest = new int[source.length];
 	    for(int i = 0; i < source.length; i++) {
 	        dest[i] = (int)source[i];
@@ -41,10 +52,90 @@ public class Polygon {
 	    return dest;
 	}
 	
-	public void rotate(double rad) {
-		
+	//get the theoretical center of any shape, this infers that xPoints and yPoints are of same size
+	public static double[] findCenter(double[] xPoints, double[] yPoints) {
+		double[] pts = new double[2];
+		double minX = getMin(xPoints);
+		double maxX = getMax(xPoints);
+		double minY = getMin(yPoints);
+		double maxY = getMax(yPoints);
+		pts[0] = (maxX + minX) / 2;
+		pts[1] = (maxY + minY) / 2;
+		return pts;
 	}
-
+	
+	//grabs an outer collider for optimization
+	public static double[][] outerColliderPoints(double[] xPoints, double[] yPoints) {
+		double[][] coords = new double[2][2];
+		double minX = getMin(xPoints);
+		double maxX = getMax(xPoints);
+		double minY = getMin(yPoints);
+		double maxY = getMax(yPoints);
+		coords[0] = new double[]{minX, maxX, maxX, minX};
+		coords[1] = new double[]{minY, minY, maxY, maxY};
+		return coords;
+	}
+	
+	//this is just to make outerColliderPoints easier to access
+	public double[][] outerColliderPoints() {
+		return outerColliderPoints(this.xPoints, this.yPoints);
+	}
+	
+	//gets the maximum value from an array
+	public static double getMax(double[] points) {
+		double max = points[0];
+		for(int i = 1; i < points.length; i++)
+			if(max < points[i]) max = points[i];
+		return max;
+	}
+	
+	//gets the minimum value from an array
+	public static double getMin(double[] points) {
+		double mix = points[0];
+		for(int i = 1; i < points.length; i++)
+			if(mix > points[i]) mix = points[i];
+		return mix;
+	}
+	
+	//Checks to see if the outer collision box is colliding with any other object
+	public ArrayList<CollisionEvent> getOuterCollisions() {
+		ArrayList<Polygon> objects = Enviroment.objects; //Can decide what to do with objects here
+		ArrayList<CollisionEvent> collisions = new ArrayList<>();
+		for(Polygon obj : objects) {
+			if(!(obj == this)) {
+			CollisionEvent ce = checkBoxCollision(this, obj);
+			if(ce != null)
+				collisions.add(ce);
+			}
+		}
+		
+		return collisions;
+	}
+	
+	//checks if there is a collision in a rectangle
+	public static CollisionEvent checkBoxCollision(Polygon p1, Polygon p2) {
+		double[][] coord1 = p1.outerColliderPoints();
+		double[][] coord2 = p2.outerColliderPoints();
+		boolean collision = true;
+		
+		for(int i = 0; i < 2; i++) {
+			if(coord1[0][0] > coord2[0][1]) collision = false;
+			if(coord1[1][0] > coord2[1][2]) collision = false;
+			
+			if(coord1[0][1] < coord2[0][0]) collision = false;
+			if(coord1[1][2] < coord2[1][0]) collision = false;
+			
+			if(collision) return new CollisionEvent(p1, p2);
+			
+			double[][] temp = coord1;
+			coord1 = coord2;
+			coord2 = temp;
+			collision = true;
+		}
+		
+		return null;
+	}
+	
 	public double[] getxPoints() {
 		return xPoints;
 	}
